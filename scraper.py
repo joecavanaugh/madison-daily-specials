c8645caa21cd56e93cdc4d272f87bdfa275cf9fd
joecavanaugh <josephf.cavanaugh@gmail.com>
2026-02-05 13:04:57 -0600

    message

--- scraper.py
+++ scraper.py
@@ -10,7 +10,7 @@ from bs4 import BeautifulSoup
 from supabase import create_client, Client
 from openai import OpenAI
 
-# --- 1. SECURE CONFIGURATION ---
+# --- 1. SECURE CONFIGURATIONs ---
 SUPABASE_URL = "https://elcikekczeaxnooncqjr.supabase.co"
 SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVsY2lrZWtjemVheG5vb25jcWpyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAyMTc0MTAsImV4cCI6MjA4NTc5MzQxMH0.JniHNE46X2GQWvPjslHFRjRo1mfKTRVeFh6iW2rF0kA"
 GROQ_API_KEY = "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"
